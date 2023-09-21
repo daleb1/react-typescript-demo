11c19d5cf2c47ea2958b1a93d0f1e4c895d8980d
Dale <dale.bagano@gmail.com>
2023-09-21 09:41:27 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+import { Greet } from './components/Greet'
+import { Person } from './components/Person'
+
+
+function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne',
+  }
+  return (
+    <div className='App'>
+      <Greet name='Dale' messageCount={20} isLoggedIn={false}/>
+      <Person name={personName} />
+    </div>
+  );
+}
+
+export default App;
