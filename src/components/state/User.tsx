81c88dc7fabbd5a3a15fa75fbe28f72784f717b6
Dale <dale.bagano@gmail.com>
2023-09-23 00:18:47 +0800

    initial commit

---
+++ src/components/state/User.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+type AuthUser = {
+    name: string
+    email: string
+}
+
+export const User = () => {
+    const [user, setUser] = useState(null)
+    const handleLogin = () => {}
+    const handleLogout = () => {}
+    return (
+        <div>
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogout}>Logout</button>
+            <div>User name is</div>
+            <div>User email is</div>
+        </div>
+    )
+}
