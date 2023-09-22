2da4900b4fb4f723a85db945a83efac3ea7bd9a2
Dale <dale.bagano@gmail.com>
2023-09-23 00:48:05 +0800

    initial commit

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,15 +6,22 @@ type AuthUser = {
 }
 
 export const User = () => {
-    const [user, setUser] = useState(null)
-    const handleLogin = () => {}
-    const handleLogout = () => {}
+    const [user, setUser] = useState<AuthUser | null>(null)
+    const handleLogin = () => {
+        setUser({
+            name: 'Dale',
+            email: 'dale@example.com',
+        })
+    }
+    const handleLogout = () => {
+        setUser(null)
+    }
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
             <button onClick={handleLogout}>Logout</button>
-            <div>User name is</div>
-            <div>User email is</div>
+            <div>User name is {user?.name}</div>
+            <div>User email is {user?.email}</div>
         </div>
     )
 }
