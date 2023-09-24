3a94407ca2150df4f2a6adf48bd71db5c61edb20
Dale <dale.bagano@gmail.com>
2023-09-25 00:20:05 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,12 @@ import { ThemeContextProvider } from './components/context/ThemeContext'
 import { Box } from './components/context/Box'
 import { UserContextProvider } from './components/context/UserContext'
 import { User } from './components/context/User'
+import { Counter } from './components/class/Counter'
+import { Private } from './components/auth/Private'
+import { Profile } from './components/auth/Profile'
+import { List } from './components/generics/List'
+import { RandomNumber } from './components/restriction/RandomNumber'
+import { Toast } from './components/templateliterals/Toast'
 
 
 function App() {
@@ -44,18 +50,44 @@ function App() {
       <Oscar>
         <Heading>The Oscar award goes to Leonardo Dicaprio!</Heading>
       </Oscar>
-      <Greet name='Dale' isLoggedIn={false}/>
+      <Greet name='Dale' isLoggedIn={false} />
       <Button handleClick={(event, id) =>
-        console.log('Button clicked', event,id)
-        }/>
+        console.log('Button clicked', event, id)
+      } />
       <Input value='' handleChange={event => console.log(event)} />
-      <Container styles={{ border: '1px solid black', padding: '1rem'}}/>
+      <Container styles={{ border: '1px solid black', padding: '1rem' }} />
       <ThemeContextProvider>
         <Box />
       </ThemeContextProvider>
       <UserContextProvider>
         <User />
       </UserContextProvider>
+      <Counter message='The count value is' />
+      <Private isLoggedIn={true} component={Profile} />
+      {/* <List
+        items={['Batman', 'Superman', 'Wonder Woman']} onClick={(item) => console.log(item)} />
+      <List
+        items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
+      <List
+        items={[
+          {
+            id: 1,
+            first: 'Bruce',
+            last: 'Wayne',
+          },
+          {
+            id: 2,
+            first: 'Clark',
+            last: 'Kent',
+          },
+          {
+            id: 3,
+            first: 'Princess',
+            last: 'Diana',
+          }
+        ]} onClick={(item) => console.log(item)} />
+      <RandomNumber value={10} isPositive />
+      <Toast position='center' />
     </div>
   );
 }
